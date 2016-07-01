98b435bbf0efb8fa87bee1c45896c6614dabec2f
srepuh <srepuh1@etf.unsa.ba>
2016-07-01 20:20:47 +0200

    Prva verzija gotova, fali dodavanje galerije i novosti

--- aschip/php/dajVozilo.php
+++ aschip/php/dajVozilo.php
@@ -4,22 +4,56 @@ include 'crud.php';
 include 'klase.php';
 
 if(isset($_REQUEST["id"])){
-	Prikazi(dajVoziloPoId($_REQUEST["id"]));
+	$vozila=dajVozilaZaModel(dajVoziloPoId($_REQUEST["id"])["model"]);
+	PrikaziSveMotore($vozila);
 }
-
-function Prikazi($vozilo){
-	echo '<div> 
-			<h2 style="margin: 20px 0;">'.dajProizvodjacaPoId($vozilo["idProizvodjaca"])["markaVozila"].'</h2>
-			<div>Model: '.$vozilo["model"].'</div>
-			<div>Motor: '.$vozilo["motor"].'</div>
-			<div>HP: '.$vozilo["hp"].'</div>
-			<div>kW: '.$vozilo["kw"].'</div>
-			<div>Snaga: '.$vozilo["snaga"].'</div>
-			<div>Obrtaji: '.$vozilo["obrtaji"].'</div>
-			<div>Cijena: '.$vozilo["cijena"].'</div>
-			<div><img src="'.dajSlikuPoid($vozilo["idSlike"])["path"].'" alt=""></div>			
-
-		</div>';	
+//cisti prikaz, to bum skontali kako cemo napraviti :D
+function PrikaziSveMotore($list){
+	echo '<table id="tabela" style="text-align:center;">
+		 <thead>
+    	<tr>
+        	<th scope="col">Vozilo</th>
+            <th scope="col">Model</th>
+            <th scope="col">Motor</th>
+            <th scope="col">HP</th>
+            <th scope="col">kW</th>
+        </tr>
+    </thead>
+    <tbody>';
+	for($i=0; $i<count($list); $i++){
+		echo '<tr onclick="dajDetaljeMotoraVozila('.$list[$i]["id"].')">
+        	<td ><a href="#">'.dajProizvodjacaPoId($list[$i]["idProizvodjaca"])["markaVozila"].'</a></td>
+            <td><a href="#">'.$list[$i]["model"].'</a></td>
+            <td><a href="#">'.$list[$i]["motor"].'</a></td>
+            <td><a href="#">'.$list[$i]["hp"].'</a></td>
+            <td><a href="#">'.$list[$i]["kw"].'</a></td>
+        </tr>';
+	}
+	
+	echo '</tbody></table>';
 }
 
 ?>
+<style>
+#tabela tbody tr:hover{
+	background-color: red;
+}
+</style>
+<script>
+function dajDetaljeMotoraVozila(id){
+	var dataRow={
+		'model':id,
+		
+	}
+	$.ajax( {
+	  url: '../php/dajDetaljeMotoraVozila.php',
+	  type: 'POST',
+	  data: dataRow,
+	  success:function(response){
+		  $("#content").html(response);
+	  }
+	});
+	return false;
+}
+
+</script>
